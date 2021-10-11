4dc3062568bb57915ae79d314eb5bba44f1b0ced
Rodrigo Branas <rodrigo.branas@gmail.com>
2021-10-11 17:34:20 -0300

    updated

--- src/Order.ts
+++ src/Order.ts
@@ -5,54 +5,54 @@ import OrderCode from "./OrderCode";
 import OrderItem from "./OrderItem";
 
 export default class Order {
-    private cpf: Cpf;
-    private coupon: Coupon | undefined;
-    private orderItems: OrderItem[];
-    private freight: number;
+	private cpf: Cpf;
+	private coupon: Coupon | undefined;
+	private orderItems: OrderItem[];
+	private freight: number;
 	code: string;
-    
-    constructor (cpf: string, readonly issueDate: Date = new Date(), readonly sequence: number = 1) {
-        this.cpf = new Cpf(cpf);
-        this.orderItems = [];
-        this.freight = 0;
+
+	constructor(cpf: string, readonly issueDate: Date = new Date(), readonly sequence: number = 1) {
+		this.cpf = new Cpf(cpf);
+		this.orderItems = [];
+		this.freight = 0;
 		const code = new OrderCode(issueDate, sequence);
 		this.code = code.value;
-    }
+	}
 
-    addItem(item: Item, quantity: number) {
-        this.freight += item.getFreight() * quantity;
-        this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
-    }
+	addItem(item: Item, quantity: number) {
+		this.freight += item.getFreight() * quantity;
+		this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+	}
 
-    addCoupon (coupon: Coupon) {
-        if (coupon.isExpired(this.issueDate)) return;
-        this.coupon = coupon;
-    }
+	addCoupon(coupon: Coupon) {
+		if (coupon.isExpired(this.issueDate)) return;
+		this.coupon = coupon;
+	}
 
-    getFreight () {
-        return this.freight;
-    }
+	getFreight() {
+		return this.freight;
+	}
 
-	getCpf () {
+	getCpf() {
 		return this.cpf.value;
 	}
 
-	getCoupon () {
+	getCoupon() {
 		return this.coupon?.code;
 	}
 
-	getOrderItems () {
+	getOrderItems() {
 		return this.orderItems;
 	}
 
-    getTotal () {
-        let total = 0;
-        for (const orderItem of this.orderItems) {
-            total += orderItem.getTotal();
-        }
-        if (this.coupon) {
-            total -= (total * this.coupon.percentage) / 100;
-        }
-        return total;
-    }
+	getTotal() {
+		let total = 0;
+		for (const orderItem of this.orderItems) {
+			total += orderItem.getTotal();
+		}
+		if (this.coupon) {
+			total -= (total * this.coupon.percentage) / 100;
+		}
+		return total;
+	}
 }
